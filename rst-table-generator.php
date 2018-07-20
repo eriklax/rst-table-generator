ab4c4670b8132c59cbaa25ae7bae5cdbe3e27ea9
Erik Lax <erik@datahack.se>
2018-07-20 10:37:47 +0200

    Create rst-table-generator.php

---
+++ rst-table-generator.php
@@ -0,0 +1,38 @@
+<?php
+
+// -b (bold first column)
+// -t (trim all values)
+// -c (csv support)
+
+$options = getopt("f:btc");
+
+$file = @file($options['f'] ?? "php://stdin");
+if (empty($file)) die("Could not open file\n");
+
+$lines = [];
+foreach($file as $f)
+{
+	if (isset($options['c']))
+		$row = str_getcsv($f);
+	else
+		$row = explode("\t", str_replace("\n", "", $f));
+	if (isset($options['t']))
+		$row = array_map(trim, $row);
+	if (isset($options['b']) && count($lines) != 0)
+		$row[0] = '**' .$row[0] . '**';
+	$lines[] = $row;
+}
+
+$max = [];
+foreach($lines as $ln => $l)
+	foreach($l as $c => $w)
+		if (strlen($w) > $max[$c]) $max[$c] = strlen($w);
+
+ksort($max);
+
+echo "+"; foreach($max as $c) echo str_repeat("-", $c + 2)."+"; echo "\n";
+foreach($lines as $ln => $l)
+{
+	echo "|"; foreach($l as $c => $w) echo " ".str_pad($w, $max[$c])." |"; echo "\n";
+	echo "+"; foreach($max as $c) echo str_repeat($ln == 0 ? "=" : "-", $c + 2)."+"; echo "\n";
+}
